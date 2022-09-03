17e4f1ffcbdc3aaf26f104f5436cf4b1a723f357
Tim Day <tday141@gmail.com>
2022-09-03 19:50:38 -0400

    Use new printer helper function

---
+++ printer.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#include "bitviewer.h"
+#include "printer.h"
+
+void show_all_info(char title[], byte_pointer start, size_t len) {
+  printf("Values for %s:\n", title);
+  printf("  On disk: ");
+  show_bytes(start, len);
+
+  printf("  In order: ");
+  show_bytes_in_order(start, len);
+
+  printf("  Bits: ");
+  print_all_bytes(start, len);
+
+  printf("\n");
+}
---
+++ printer.h
@@ -0,0 +1,3 @@
+typedef unsigned char *byte_pointer;
+
+void show_all_info(char title[], byte_pointer start, size_t len);
